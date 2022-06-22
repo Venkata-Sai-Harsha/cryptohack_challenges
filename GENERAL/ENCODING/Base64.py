ba6c78d913ff65c057b9ef7e601c917ce8f03bcd
sai harsha <94219897+Venkata-Sai-Harsha@users.noreply.github.com>
2022-06-22 11:45:55 +0530

    Create Base64.py

---
+++ GENERAL/ENCODING/Base64.py
@@ -0,0 +1,9 @@
+import base64
+hex="72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
+data = bytes.fromhex(hex)
+flag=base64.b64encode(data)
+print(flag)
+
+
+#b'crypto/Base+64+Encoding+is+Web+Safe/'
+#b'crypto{Base_64_Encoding_is_Web_Safe}'
