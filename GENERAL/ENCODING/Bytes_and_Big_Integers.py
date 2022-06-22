573942dea5fa1fca52e31a8b3aee12b18e4e957c
sai harsha <94219897+Venkata-Sai-Harsha@users.noreply.github.com>
2022-06-22 11:50:49 +0530

    Create Bytes_and_Big_Integers.py

---
+++ GENERAL/ENCODING/Bytes_and_Big_Integers.py
@@ -0,0 +1,8 @@
+from Crypto.Util.number import bytes_to_long
+from Crypto.Util.number import long_to_bytes
+
+data = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+bytes = long_to_bytes(data)
+print(bytes)
+
+#b'crypto{3nc0d1n6_4ll_7h3_w4y_d0wn}'
